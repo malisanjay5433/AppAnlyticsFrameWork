2463243fe8e8b875cf34331bcd01193c4baa23c6
Sanjay Mali <malisanjay5433@gmail.com>
2018-01-06 19:15:48 +0530

    Initial Commit

---
+++ AppAnlytics/AppAnlytics.h
@@ -0,0 +1,19 @@
+//
+//  AppAnlytics.h
+//  AppAnlytics
+//
+//  Created by Sanjay Mali on 06/01/18.
+//  Copyright © 2018 Sanjay Mali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppAnlytics.
+FOUNDATION_EXPORT double AppAnlyticsVersionNumber;
+
+//! Project version string for AppAnlytics.
+FOUNDATION_EXPORT const unsigned char AppAnlyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppAnlytics/PublicHeader.h>
+
+
